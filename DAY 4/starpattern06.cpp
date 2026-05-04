970973d9608229e0991be515b7e97a6874ee8aa4
venkat23bcon0248 <venkat.23bcon0248@jecrcu.edu.in>
2026-05-04 15:57:10 +0530

    commit

--- DAY 4/starpattern06.cpp
+++ DAY 4/starpattern06.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 
 int main() {
-    int n = 5;
+    int n;
+    cout << "enter the number of rows";
+    cin >> n;
 
     for(int i = 1; i <= n; i++) {
         for(int j = 1; j <= n - i; j++)
