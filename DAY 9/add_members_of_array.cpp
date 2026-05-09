1b24798cf4ecec63749d85f8480b807e32b8d690
venkat23bcon0248 <venkat.23bcon0248@jecrcu.edu.in>
2026-05-09 15:44:21 +0530

    commit

--- DAY 9/add_members_of_array.cpp
+++ DAY 9/add_members_of_array.cpp
@@ -1,6 +1,7 @@
 # include <iostream>
 using namespace std;
 int main(){
+    
     int arr [5] = {1,2,3,4,5};
     int answer = 0;
     for ( int i = 0; i<5; i++){
---
+++ DAY 9/reverse_an_array.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++) {
+        cout << "Enter elements: ";
+        cin >> arr[i];
+    }
+
+    int i = 0;
+    int j = n - 1;
+    int temp;
+
+    while(i < j) {
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+
+        i++;
+        j--;
+    }
+
+    cout << "Reversed Array: ";
+
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
