cbbc1d01a7bf9e7403f72acd4232719c888a4f06
venkat23bcon0248 <venkat.23bcon0248@jecrcu.edu.in>
2026-06-24 11:23:49 +0530

    commit

---
+++ PHASE_II/DAY 05/205. Isomorphic Strings.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string s,t;
+    cout << "Enter first string: ";
+    cin >> s;
+    cout << "Enter second string: ";
+    cin >> t;
+    
+}
